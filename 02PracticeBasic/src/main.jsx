1789e99c20da90b32a7d2aab7dbef62b84ac5f28
Parshant124 <parsh3456@gmail.com>
2026-01-13 00:17:38 +0530

    lecture 4

--- 02PracticeBasic/src/main.jsx
+++ 02PracticeBasic/src/main.jsx
@@ -1,13 +1,27 @@
 import { StrictMode } from 'react'
+import React from "react";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import PracFunc from './Custom'
 
+const customElement = (
+  <h1>This is a customElement, and it is a object but syntax is different</h1>
+)
+
+// creating custom react element
+const CustomElem = React.createElement(
+  'a', // define type
+  {href: 'https://www.google.com', target: '_blank'}, // set Attributes, leave{} empty if no attributes
+  'click here' // direct Text
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <>
+
+<StrictMode>
+  <>
     <App />
-    <PracFunc />
-    </>
-  </StrictMode>,
+    <PracFunc /> 
+    {CustomElem}
+  </>
+  </StrictMode>
 )
