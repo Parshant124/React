a8e65bf6e65d3f8785b1b5cad06d51ec305adbeb
Parshant124 <parsh3456@gmail.com>
2026-01-14 23:27:24 +0530

    tailwind + props

---
+++ 05TailwindProp/src/components/Card.jsx
@@ -0,0 +1,41 @@
+function Card({name, role, ID}){
+
+    return(
+        <>
+            <div className="w-80 bg-green-400 rounded-xl shadow-lg p-6 text-center">
+        
+        {/* Profile Image */}
+        <img
+          src="https://images.pexels.com/photos/18465582/pexels-photo-18465582.jpeg"
+          alt="Profile"
+          className="w-24 h-24 mx-auto rounded-full border-4 border-blue-500"
+        />
+
+        {/* Name */}
+        <h2 className="mt-4 text-xl font-bold text-gray-800">
+          {name}
+        </h2>
+
+        {/* Role */}
+        <p className="text-sm text-gray-500">
+          {role}
+        </p>
+
+        {/* Divider */}
+        <div className="my-4 h-px bg-gray-200"></div>
+
+        {/* Info */}
+        <p className="text-sm text-gray-600">
+          ID: {ID}
+        </p>
+
+        <p className="text-sm text-gray-600">
+          Email: parshant@example.com
+        </p>
+
+      </div>
+        </>
+    )
+}
+
+export default Card;
---
+++ 05TailwindProp/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Card from './components/Card.jsx'
+import Card2 from './components/Card2.jsx'
+
+const myArr = [1,2,3,4,5]
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Card name="Parshant" role="Frontend Developer" ID="2026DEV01"/>
+    <Card name="React-Vite" role="Making websites" ID="2137824"/>
+    <Card2 name="Parshant" role="Frontend Developer" ID="2026DEV01"/>
+    <Card2 name="React-Vite" role="Making websites" ID="2137824"/>
+  </StrictMode>,
+)
