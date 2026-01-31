2f45f27d7a33e2bc97aba06f93ba93d056bae9fa
Parshant124 <parsh3456@gmail.com>
2026-02-01 01:01:51 +0530

    bg changed

--- 00Summary/src/Summary/Summary05.jsx
+++ 00Summary/src/Summary/Summary05.jsx
@@ -5,17 +5,17 @@ export default function Summary05(){
         <>
         <div>
             <div
-            className="text-center text-2xl font-bold text-orange-800 bg-gray-200"
+            className="text-center text-2xl font-bold text-orange-800 bg-gray-400"
             >
                 <h1 className="">Day 5 : Password Generator</h1>
             </div>
-            <div>
+            <div className="text-white">
                 <h1 className="font-semibold">About Project</h1>
                 <h1 className="mx-4"> A simple password generator where user set length and two attributes(NumberPresent, SymbolPresent) and a copy button</h1>
                 <h2 className="font-semibold">Hooks Used:</h2>
                 <h2 className="mx-4">useState, useCallback, useEffect, useRef</h2>
             </div>
-            <div>
+            <div className="text-white">
                 <h1 className="text-center font-bold text-xl p-4">Working</h1>
                 <h2><strong>Password Generation:</strong> Password is generated on the basis of (length, NumPresent, SymbolPresent), done via the use of Math.random</h2>
                 <h2><strong>useState: </strong> The generated password is then stored via setPass call of useState which is then appended in the h1 tag of display</h2>
